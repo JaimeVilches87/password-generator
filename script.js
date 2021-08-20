6c0e40a23da7d85675de95de352d61da2d2bf585
JaimeVilches87 <jaimevilches87@gmail.com>
2021-08-20 19:56:56 -0400

    added generate password function

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // Assignment code here
+function generatePassword() {
 
+  var specialSet = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
-var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var numSet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var lowerCaseSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var upperCaseSet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var selectedArray = [];
 
+  var passwordLength = getPasswordLength();
 
+  var charTypeSelected = false;
+  // This loop ensures the user selects at least one character type
+  while (charTypeSelected == false) {
+    var lowerCase = getChoice("lowercase");
+    var upperCase = getChoice("uppercase");
+    var numericCharacters = getChoice("numeric");
+    var specialCharacters = getChoice("special");
+    if ((lowerCase) || (upperCase) || (numericCharacters) || (specialCharacters)) {
+      charTypeSelected = true;
+    } else {
+      window.alert("You must select at least one character type.")
+    }
+  }
 
+  
 
 
 // Get references to the #generate element
